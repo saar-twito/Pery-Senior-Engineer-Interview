8f0bb5abc031717363d58af0da6fcbcdd964ef25
saartwito <saartwito49@gmail.com>
2024-10-08 11:18:51 +0300

    Update base URL in axiosConfig.ts to use the new backend URL

--- client/src/axiosConfig.ts
+++ client/src/axiosConfig.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance with a base URL
 const axiosInstance = axios.create({
-  baseURL: 'https://server-4934.onrender.com',  // Replace with your backend URL if different
+  baseURL: 'https://server-4934.onrender.com', // backend server URL
   headers: {
     'Content-Type': 'application/json',
   },
